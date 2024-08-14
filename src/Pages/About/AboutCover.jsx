3b225db3f01ea1724585a888257f785d98495568
Jaheda20 <jahedasultana20@gmail.com>
2024-08-14 18:13:21 +0200

    initialized project autolynx

--- src/Pages/About/AboutCover.jsx
+++ src/Pages/About/AboutCover.jsx
@@ -6,7 +6,7 @@ const AboutCover = () => {
             className="hero h-[700px]"
             style={{
                 backgroundImage: `linear-gradient(to top, rgba(21, 21, 21, 0.65), rgba(21, 21, 21, 0.65)), 
-                                  url('https://i.ibb.co/fXSqw74/photo-1596792598780-6777ec5b57ee-q-80-w-1469-auto-format-fit-crop-ixlib-rb-4-0.jpg')`,
+                                  url('https://i.ibb.co/dmxCD2d/photo-1516733968668-dbdce39c4651-q-80-w-1528-auto-format-fit-crop-ixlib-rb-4-0.jpg')`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat'
